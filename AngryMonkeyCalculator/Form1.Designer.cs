a956fd0f633f401fdf9a0545dc2d8788325ac5a0
Karam Alhamoud <karam.developer@gmail.com>
2021-03-05 15:11:39 +0200

    Add project files.

---
+++ AngryMonkeyCalculator/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace AngryMonkeyCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(82, 33);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(183, 38);
+            this.input.TabIndex = 0;
+            this.input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(323, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.ForeColor = System.Drawing.Color.White;
+            this.result.Location = new System.Drawing.Point(12, 104);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(323, 53);
+            this.result.TabIndex = 3;
+            this.result.Text = "Result";
+            this.result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(347, 250);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.85D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AngryMonkeyCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ AngryMonkeyCalculator/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace AngryMonkeyCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calc(input.Text);
+        }
+
+
+        private void Calc(string input)
+        {
+            var parser = Regex.Split(input, @"\s*([-+/*])\s*").Where(n => !string.IsNullOrEmpty(n)).ToList();
+            try
+            {
+                int _value1 = int.Parse(parser[0]);
+                var _operator = parser[1];
+                int _value2 = int.Parse(parser[2]);
+
+
+                int CalcResult = 0;
+
+                if (_operator == "+")
+                    CalcResult = _value1 + _value2;
+                else if (_operator == "-")
+                    CalcResult = _value1 - _value2;
+                else if (_operator == "*")
+                    CalcResult = _value1 * _value2;
+                else if (_operator == "/")
+                    CalcResult = _value1 / _value2;
+
+
+                result.ForeColor = Color.White;
+                result.Text = $"{_value1} {_operator} {_value2} = {CalcResult}";
+            }
+            catch
+            {
+                result.ForeColor = Color.Red;
+                result.Text = "Invalid Format";
+            }
+        }
+    }
+}
